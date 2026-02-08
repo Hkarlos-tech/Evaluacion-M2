e1fc8bff9c5e828c988629c8b747fcd9dc088dfa
Carlos Huenchual <huenchual.carlos@gmail.com>
2026-02-08 03:52:36 -0300

    se agrega animacion de las secciones al hacer scroll

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -12,3 +12,32 @@ btnTema.addEventListener("click", () => {
     btnTema.textContent = "Modo oscuro";
   }
 });
+
+// Animacion al hacer scroll JQuery 
+
+
+$(document).ready(function () {
+
+  // Ocultamos las secciones al inicio
+  $("section").css({
+    opacity: 0,
+    transform: "translateY(30px)"
+  });
+
+  // Detecta el scroll
+  $(window).on("scroll", function () {
+    $("section").each(function () {
+      const posicionElemento = $(this).offset().top;
+      const posicionScroll = $(window).scrollTop() + $(window).height() - 100;
+
+      if (posicionScroll > posicionElemento) {
+        $(this).animate(
+          { opacity: 1 },
+          800
+        ).css("transform", "translateY(0)");
+      }
+    });
+  });
+
+});
+
